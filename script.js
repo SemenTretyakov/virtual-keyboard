77834ed8e25eaae64ab0f47303a7d2e63df35443
Semen Tretyakov <tretakovs295@gmail.com>
2023-04-29 17:10:11 +0500

    add: make up action by clicking on the mouse and keys

--- script.js
+++ script.js
@@ -20,13 +20,21 @@ container.prepend(sectionTitle, textarea, keyboardWrapper);
 
 
 const keyboard = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Alt', ' ', 'Alt', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Meta',];
+const keyboardUp = keyboard.map((letter) => {
+    if (letter.length <= 1) {
+        return letter.toUpperCase()
+    } else {
+        return letter
+    }
+    
+})
 
 // document.addEventListener('keydown', (event) => {
 //     keyboard.push(event.key)
 //     console.log(keyboard)
 // })
 
-keyboard.forEach((key) => {
+keyboardUp.forEach((key) => {
     const keyElement = document.createElement('button');
     keyElement.textContent = key;
     keyElement.className = 'key key__text';
@@ -51,11 +59,10 @@ keyboard.forEach((key) => {
         keyElement.classList.add('key_slash-tab');
     } else if (key === 'CapsLock') {
         keyElement.classList.add('key_caps');
-        keyElement.textContent = 'Caps';
     } else if (key === 'Enter') {
         keyElement.classList.add('key_enter');
     } else if (key === 'Shift') {
-        keyElement.classList.add('key_shift-left');
+        keyElement.classList.add('key_shift');
     } else if (key === 'Control') {
         keyElement.classList.add('key_ctrl');
         keyElement.textContent = 'Ctrl';
@@ -65,6 +72,66 @@ keyboard.forEach((key) => {
     } else if (key === 'Alt') {
         keyElement.classList.add('key_alt');
     }
-
     keyboardWrapper.appendChild(keyElement);
+})
+
+const keys = document.querySelectorAll('.key');
+const spaceKey = document.querySelector('.key_space');
+const shift_left = document.querySelector('.key_shift');
+const shift_right = document.querySelector('.key_shift-right');
+const caps_lock_key = document.querySelector('.key_caps');
+const text_input = document.querySelector('.text');
+
+keys.forEach((key) => {
+    key.setAttribute('keyname', key.innerText);
+    key.setAttribute('lowerCaseName', key.innerText.toLowerCase());
+})
+
+document.addEventListener('keydown', (event) => {
+    keys.forEach((key) => {
+        if (event.key === key.getAttribute('keyname') || event.key === key.getAttribute('lowerCaseName')) {
+            key.classList.add('key_active')
+        }
+        if (event.code === 'Space') {
+            spaceKey.classList.add('key_active')
+        }
+        if (event.code === 'Shift') {
+            shift_right.classList.remove('key_active')
+        }
+        if (event.code === 'Capslock') {
+            caps_lock_key.classList.toggle('key_active');
+        }
+    })
+})
+
+document.addEventListener('keyup', function(event) {
+    keys.forEach((key) => {
+        if (event.key === key.getAttribute('keyname') || event.key === key.getAttribute('lowerCaseName')) {
+            key.classList.remove('key_active')
+            key.classList.add('remove')
+        }
+        if (event.code === 'Space') {
+            spaceKey.classList.remove('key_active');
+            spaceKey.classList.add('remove');
+        }
+        if (event.code === 'Shift') {
+            shift_right.classList.remove('key_active')
+            shift_right.classList.remove('remove')
+        }
+        setTimeout(() => {
+            key.classList.remove('remove')
+        }, 250)
+    })
+})
+
+keys.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+        keys.forEach((btn) => {
+            btn.classList.remove('key_active');
+        });
+        const { target } = event;
+        const targetClos = target.closest('.key');
+        targetClos.classList.add('key_active');
+        text_input.append(targetClos.textContent);
+    })
 })
